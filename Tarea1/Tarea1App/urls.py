31a3aa3ec74e345edc9f6617409d71af8a281601
Cristobal Gonzalez <cgonz@uc.cl>
2021-09-09 00:43:52 -0300

    terminando

--- Tarea1/Tarea1App/urls.py
+++ Tarea1/Tarea1App/urls.py
@@ -4,6 +4,10 @@ from Tarea1App import views
 
 urlpatterns = [
     path('', views.home, name='Home'),
-    path('usuarios/', views.usuarios, name='Usuarios'),
-    path('ciudades/', views.ciudades, name='Ciudades'),
+    path('usuarios/<int:page>', views.usuarios, name='Usuarios'),
+    path('ciudades/<int:page>', views.ciudades, name='Ciudades'),
+    path('usuario/<int:id>', views.usuario, name='Usuario'),
+    path('ciudad/<str:name>', views.ciudad, name='Ciudad'),
+    path('buscar_usuario/', views.buscar_usuario, name='Buscar_usuario'),
+    path('buscar_ciudad/', views.buscar_ciudad, name='Buscar_ciudad'),
 ]
